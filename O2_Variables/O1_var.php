49e7c8596c8b57b3f1fb022950da9c5d47623613
Farhanhussain7 <farhanhussain81303@gmail.com>
2025-07-30 23:40:33 +0530

    Add new files for variables and html

--- O2_Variables/O1_var.php
+++ O2_Variables/O1_var.php
@@ -1,4 +1,38 @@
 <!--It variables are storage location .
     It is abstracted storage location 
     It can store any kind of data 
-    ex : name, number, flaot , -->
+    ex : name, number, flaot ,
+    varaible will be with $ sign 
+    there is no need to declare type and they change also 
+
+Rules :- a variable start with the $ sign
+         a variable name cannot start with a number 
+         a variable name must start with a letter or the underscore(_) character.
+         a variable name cannot start with the a special operator 
+         a variable can only contains aplha-numeric characters and underscore(_) .
+         example :- $name, $Name ,$NAME are three different variables.
+         syntax:- $name="FarhanHussain";
+     -->
+<?php
+     $name = "Farhan ";
+     $Name = "Farhan Hussain";
+     $_age_=22;
+echo "Name is $name full name is $Name and my age is $_age_ <br/>";
+
+$num=20;
+$a=$b=30;
+echo "Value of $b";
+echo $num*3;
+echo "<br/>";
+
+
+echo $num+88;
+echo "<br/>";
+
+
+echo $num-8;
+echo "<br/>";
+     
+?>
+
+<!-- C:\xampp\htdocs\PHP\O2_Variables/O1_var.php -->
---
+++ O3_HTML_/O1_Php_with_Html.php
@@ -0,0 +1,46 @@
+<!-- Php - it will handle the server and batabase part
+    html - it will handle the frontend with static ui -->
+
+
+    <!-- First way  -->
+<?php
+
+    echo "<H1 style='color:green'>Helloe my name is Farhan </H1>";
+    echo "<H3 style='color:blue'>I am From Delhi </H3>";
+
+?>
+
+<!-- Second way  -->
+<?php
+    $name="Farhan hussian";
+    echo "<h1 style='color:yellow'>My name is $name</h1>"
+?>
+
+<!-- Third way  -->
+<H1 style='color:red'>
+    Hello how are you !
+    <?php
+
+    echo "$name : Im fine ";
+    ?>
+</H1>
+
+
+
+
+<!-- Fourth way  -->
+<?php
+
+$colorHead="orange";
+
+?>
+<H2 style='color:<?php echo $colorHead ; ?>' ><?php echo "This is different method " ;?> </H2>
+<H2 style='color:<?php echo $colorHead ; ?>' ><?php echo "Single variable change many place "; ?> </H2>
+<H2 style='color:<?php echo $colorHead ; ?>' ><?php echo "This is different way of using color "; ?> </H2>
+
+
+    <!-- C:\xampp\htdocs\PHP\O3_HTML_\O1_Php_with_Html.php -->
+
+
+
+
